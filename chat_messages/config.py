035b20aa2ed24dcc1c43840ce804949e28fd2a95
G Sravan Kumar <sravan335@gmail.com>
2018-05-29 14:28:50 +0530

    Change processed file to csv format

--- chat_messages/config.py
+++ chat_messages/config.py
@@ -1,3 +1,3 @@
 ORIGINAL_MESSAGES_PATH = '/Users/sravan/Desktop/projects/chat-messages/chat_messages/data/telegram_messages.txt'
-PROCESSED_MESSAGES_PATH = '/Users/sravan/Desktop/projects/chat-messages/chat_messages/data/processed_telegram_messages.txt'
+PROCESSED_MESSAGES_PATH = '/Users/sravan/Desktop/projects/chat-messages/chat_messages/data/processed_telegram_messages.csv'
 CSV_SEP = '\x01'
