64d4f5b00eae9a8a008c5d4d10787ae7302e0d6a
G Sravan Kumar <sravan335@gmail.com>
2018-05-29 16:11:53 +0530

    Initial class definitions for message and person

---
+++ chat_messages/person.py
@@ -0,0 +1,24 @@
+"""
+Module which contains class definition of a person.
+
+This module contains the Person class which takes the name and messages dataframe sent by a person
+and calculates relevant statistics like number of messages sent, messages on specific weekdays, etc.
+"""
+
+
+class Person(object):
+    """
+    Class to represent a person and messages sent by him/her
+
+    Attributes:
+        name (str): Name of the person
+    """
+    def __init__(self, name):
+        """
+        Constructor to define the name and read list of messages sent by the person
+
+        Args:
+             name (str): Name of the person
+             messages (list): List of message objects
+        """
+        self.name = name
